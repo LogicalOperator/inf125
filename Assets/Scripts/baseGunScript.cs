00682e523e272bb8ef2985f5f1784fe5c605372a
Alex <agscott1@uci.edu>
2016-10-10 00:44:14 -0700

    Added fireRate to guns so left-click can be held down

--- Assets/Scripts/baseGunScript.cs
+++ Assets/Scripts/baseGunScript.cs
@@ -5,23 +5,28 @@ public class baseGunScript : MonoBehaviour {
     //base stuff needed for a gun
     public AudioClip gunSound;
     public GameObject bulletPrefab;
+
     public string gunType;
     public int dmgMod = 0;
     public string gunName;
     public Sprite gunImage;
-	
+    public float fireRate; // fire rate for the bullet
+
+    private float nextFire = 0.0f;
 
     public virtual void summonBullet()
     {
-        if (Input.GetMouseButtonDown(0)) //when ever mouse left mouse button is clicked down 
+        if (Input.GetMouseButton(0) && Time.time > nextFire) // when ever mouse left mouse button is clicked down 
         {
+            nextFire = Time.time + fireRate; // only displays new bullet when correct amount of time has passed
+
             AudioSource.PlayClipAtPoint(gunSound, Camera.main.transform.position, 10f);
-            Vector3 pos = Input.mousePosition; //obtain mousepostion
-            pos = Camera.main.ScreenToWorldPoint(pos);//obtain exact mouse position from main camera screen
+            Vector3 pos = Input.mousePosition; // obtain mousepostion
+            pos = Camera.main.ScreenToWorldPoint(pos); // obtain exact mouse position from main camera screen
             pos.z = transform.position.z;
 
             Quaternion q = Quaternion.FromToRotation(Vector3.up, pos - transform.position);
-            GameObject go = Instantiate(bulletPrefab, transform.position, q) as GameObject;//create bullet as a new gameObject
+            GameObject go = Instantiate(bulletPrefab, transform.position, q) as GameObject; // create bullet as a new gameObject
         }
     }
 }
--- Assets/Scripts/initialGun.cs
+++ Assets/Scripts/initialGun.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class initialGun : baseGunScript {
 
-	// Use this for initialization
-	void Start () {
-        gunName = "Pistol"; //update gunname, dmg mod, and other things here
+    // Use this for initialization
+    void Start () {
+        gunName = "Pistol"; //update gunname, dmg mod, firing rate and other things here
+        fireRate = 0.1f;
 	}
 	
 	// Update is called once per frame
