bcf477447f947f1f43d611d2f235857fdbb7eec3
Jason Parsons <jparsons@uci.edu>
2016-11-22 23:14:27 -0800

    Mapped shooting controls to I J K L keys.
    Based on William's code. Updated and refactored to integrate existing mouse controls. Also mapped secondary fire controls to same keys when holding RightShift key.

--- Assets/Scripts/machineGun.cs
+++ Assets/Scripts/machineGun.cs
@@ -20,29 +20,50 @@ public class machineGun : baseGunScript {
         specialFire();
 	}
 
-    public override void specialFire()//speical fire for max resource
-    {
-        if (Input.GetMouseButtonDown(1))
-        {
-            if (player.GetComponent<controller>().currentDark < 100)//check if dark light, if not play not full sound
-            {
-                AudioSource.PlayClipAtPoint(emptyGunSound, Camera.main.transform.position, 10f);
-            }
-            else
-            {
-                player.GetComponent<controller>().resetBars();//reset both resources
-                AudioSource.PlayClipAtPoint(gunSound, Camera.main.transform.position, 10f);
-                Vector3 pos = Input.mousePosition; // obtain mousepostion
-                pos = Camera.main.ScreenToWorldPoint(pos); // obtain exact mouse position from main camera screen
-                pos.z = transform.position.z;
+	public override void specialFire()//speical fire for max resource
+	{
 
-                Quaternion q = Quaternion.FromToRotation(Vector3.up, pos - transform.position);
-                GameObject go = Instantiate(bulletPrefab, transform.position, q) as GameObject; // create bullet as a new gameObject
-                go = Instantiate(bulletPrefab, transform.position-new Vector3(0.5f,0,0), q) as GameObject; // create clones
-                go = Instantiate(bulletPrefab, transform.position+new Vector3(0.5f,0,0), q) as GameObject;
+		Vector3 pos = new Vector3();
+		Vector3 rotation_to = new Vector3();
 
-            }
-        }
-    }
+		if (Input.GetMouseButton(1) || (Input.GetKey(KeyCode.RightShift) && (Input.GetKey(KeyCode.J) || Input.GetKey(KeyCode.L) || Input.GetKey(KeyCode.I) || Input.GetKey(KeyCode.K)))) {
+			if (player.GetComponent<controller>().currentDark < 100)//check if max light, if not play not full sound
+			{
+				audioManager.instance.playSound(emptyGunSound, transform.position);
+			}
+			else
+			{
+				if (Input.GetMouseButton (1)) {
+					pos = Input.mousePosition; // obtain mousepostion
+					pos = Camera.main.ScreenToWorldPoint (pos); // obtain exact mouse position from main camera screen
+					pos.z = transform.position.z;
+
+					rotation_to = pos - transform.position;
+				}
+				else {
+					if (Input.GetKey (KeyCode.J)) {
+						rotation_to = Vector3.left;
+					} 
+					if (Input.GetKey (KeyCode.L)) {
+						rotation_to += Vector3.right;
+					} 
+					if (Input.GetKey (KeyCode.I)) {
+						rotation_to += Vector3.up;
+					}
+					if (Input.GetKey (KeyCode.K)) {
+						rotation_to += Vector3.down;
+					}
+				}
+
+				player.GetComponent<controller>().resetBars(); //reset both resources
+				audioManager.instance.playSound(gunSound, transform.position);
+
+				Quaternion q = Quaternion.FromToRotation(Vector3.up, rotation_to);
+				GameObject go = Instantiate(bulletPrefab, transform.position, q) as GameObject; // create bullet as a new gameObject
+				go = Instantiate(bulletPrefab, transform.position - new Vector3(0.5f, 0, 0), q) as GameObject; // create clones
+				go = Instantiate(bulletPrefab, transform.position + new Vector3(0.5f, 0, 0), q) as GameObject;
+			}
+		}
+	}
 
 }
