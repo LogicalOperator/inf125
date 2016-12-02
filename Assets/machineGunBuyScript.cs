0152103c944f6dcd85239ad18cb2c2db05eb3a56
Derek Chow <dtchow@uci.edu>
2016-12-02 02:45:21 -0800

    goldChanges

--- Assets/machineGunBuyScript.cs
+++ Assets/machineGunBuyScript.cs
@@ -6,6 +6,7 @@ public class machineGunBuyScript : buyableBase {
 	// Use this for initialization
 	void Start () {
         gunIndex = 3;
+        goldCost = 30;
         gunType = "dark";
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<controller>();
         this.transform.GetChild(0).GetComponent<Text>().text = " "+goldCost;
