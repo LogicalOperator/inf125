4755276fbed9a16bae37a8465ffe38c48292b26d
William Tjiptadjaja <William Tjiptadjaja>
2016-12-06 12:31:40 -0800

    reset.cs

--- Assets/Scripts/reset.cs
+++ Assets/Scripts/reset.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class reset : MonoBehaviour {
-
+	public audioManager audio;
 	// Use this for initialization
 	void Start () {
         PlayerPrefs.DeleteKey("score");
@@ -10,6 +11,14 @@ public class reset : MonoBehaviour {
         PlayerPrefs.DeleteKey("winCondition");
         PlayerPrefs.DeleteKey("primaryGunIndex");
         PlayerPrefs.DeleteKey("secondaryGunIndex");
+		
+		 StartCoroutine(timerCutscene());
     }
+	
+	IEnumerator  timerCutscene(){
+		yield return new WaitForSeconds(120);
+		Destroy(audio.gameObject);
+		SceneManager.LoadScene("cutScene");
+	}
     
 }
